a34cf55b15eff5f94eae9b2d2e7bf6b1d7a4dced
vperizes <vperizes@gmail.com>
2023-03-18 21:54:18 -0500

    New Smash power up
    Added new power up type that allows player to jump up and smash down on platform to kill enemies

--- Prototype 4_Wrecking Balls/Assets/Scripts/PlayerController.cs
+++ Prototype 4_Wrecking Balls/Assets/Scripts/PlayerController.cs
@@ -25,11 +25,20 @@ public class PlayerController : MonoBehaviour
     public bool hasExtraPowerup = false;
     public bool hasSmashPowerup = false;
 
+
+    private Vector3 powerupIndicatorOffset;
     public float speed;
     private int yBound = -3;
     private float forwardInput;
-    private float powerupStrength = 15.0f;
-    private Vector3 powerupIndicatorOffset;
+    private float powerupStrength = 15.0f; //var for pushback powerup
+
+    [SerializeField] float explosionRadius; //var for smash powerup
+    [SerializeField] float explosionForce; //var for smash powerup
+    [SerializeField] float hangTime; //var for smash powerup
+    [SerializeField] float jumpSpeed; //var for smash powerup
+    [SerializeField] float smashSpeed; //var for smash powerup
+    private float floorY; //var for smash powerup
+
 
     // Start is called before the first frame update
     void Start()
@@ -58,20 +67,21 @@ public class PlayerController : MonoBehaviour
         {
             LaunchRockets();
         }
-
-        if (currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space))
+        if (currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !hasSmashPowerup)
         {
-            Smash();
+            hasSmashPowerup = true;
+            StartCoroutine(Smash());
+
         }
 
-        //Defines game over if player off platform
+        //Defines game over if player is off platform
 
         if (transform.position.y < yBound)
         {
             Destroy(gameObject);
             gameManager.GameOver();
         }
-       
+
     }
 
 
@@ -111,10 +121,15 @@ public class PlayerController : MonoBehaviour
 
         if (other.CompareTag("SmashPowerup"))
         {
-            hasSmashPowerup = true;
+            //hasSmashPowerup = true;
             currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             smashPowerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
+
+            if (StartCoroutine(PowerupCountdownRoutine()) != null)
+            {
+                StopCoroutine(PowerupCountdownRoutine());
+            }
         }
 
     }
@@ -141,6 +156,8 @@ public class PlayerController : MonoBehaviour
         {
             yield return new WaitForSeconds(6);
             hasSmashPowerup = false;
+            currentPowerUp = PowerUpType.None;
+            smashPowerupIndicator.SetActive(false);
         }
 
     }
@@ -158,19 +175,57 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    //Rocket power up functionality
     void LaunchRockets()
     {
         foreach (var enemy in FindObjectsOfType<Enemy>())
         {
             tmpRocket = Instantiate(projectilePrefab, transform.position + Vector3.forward, Quaternion.identity);
+
+            //calls Fire method from projectile behaviour cs
             tmpRocket.GetComponent<ProjectileBehaviour>().Fire(enemy.transform);
         }
     }
 
-    void Smash()
+    //Smash Power up functionality
+
+    IEnumerator Smash()
     {
-        playerRb.AddForce(Vector3.up * 20f, ForceMode.Impulse);
+        //Store the y position before taking off
+        floorY = transform.position.y;
+
+        //Calculate the amount of time we will go up
+        float jumpTime = Time.time + hangTime;
+
+        while (Time.time < jumpTime)
+        {
+            //move the player up while still keeping their x velocity.
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpSpeed);
+            yield return null;
+        }
+
+        //Now move the player down
+        while (transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed);
+            yield return null;
+        }
+
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        //cycle through all active enemies
+        for(int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i] != null)
+            {
+                enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+            }
+        }
+
+        hasSmashPowerup = false;
     }
+
+
 }
 
 
